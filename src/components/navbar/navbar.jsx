29edd38ab43cdee2c6948722d8bccb5c17b60691
vaidi-p44 <parmarvaidehi30@gmail.com>
2025-01-04 22:17:06 +0530

    i change the M to m

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,25 +3,33 @@ import logo from "../../assets/mylogo.png";
 import { useRef, useState } from "react";
 import underline from "../../assets/line.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBars,faXmark} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [menu, setmenu] = useState("home");
-  const menuref=useRef();
-  const openmenu=()=>{
-    menuref.current.style.right="0";
-  }
-  const closemenu=()=>{
-    menuref.current.style.right="-350px";
-  }
+  const menuref = useRef();
+  const openmenu = () => {
+    menuref.current.style.right = "0";
+  };
+  const closemenu = () => {
+    menuref.current.style.right = "-350px";
+  };
   return (
     <>
       <div className={styles.navbar}>
         <img src={logo} alt="logo" />
-        <FontAwesomeIcon icon={faBars} className={styles.navopen} onClick={openmenu} />
+        <FontAwesomeIcon
+          icon={faBars}
+          className={styles.navopen}
+          onClick={openmenu}
+        />
         <ul ref={menuref} className={styles.navmenu}>
-        <FontAwesomeIcon icon={faXmark} className={styles.navclose} onClick={closemenu}/>
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={styles.navclose}
+            onClick={closemenu}
+          />
           <li>
             <AnchorLink className={styles.anchorlink} href="#home">
               <p onClick={() => setmenu("home")}>Home</p>
@@ -30,12 +38,16 @@ const Navbar = () => {
           </li>
           <li>
             <AnchorLink className={styles.anchorlink} offset={50} href="#about">
-              <p onClick={() => setmenu("about")}>About Me</p>
+              <p onClick={() => setmenu("about")}>About me</p>
             </AnchorLink>
             {menu === "about" ? <img src={underline} alt="" /> : <></>}
           </li>
           <li>
-            <AnchorLink className={styles.anchorlink} offset={50} href="#services">
+            <AnchorLink
+              className={styles.anchorlink}
+              offset={50}
+              href="#services"
+            >
               <p onClick={() => setmenu("services")}>Services</p>
             </AnchorLink>
             {menu === "services" ? <img src={underline} alt="" /> : <></>}
@@ -47,13 +59,22 @@ const Navbar = () => {
             {menu === "work" ? <img src={underline} alt="" /> : <></>}
           </li>
           <li>
-            <AnchorLink className={styles.anchorlink} offset={50} href="#contact">
+            <AnchorLink
+              className={styles.anchorlink}
+              offset={50}
+              href="#contact"
+            >
               <p onClick={() => setmenu("contact")}>Contact</p>
             </AnchorLink>
             {menu === "contact" ? <img src={underline} alt="" /> : <></>}
           </li>
         </ul>
-        <div className={styles.navconnect}> <AnchorLink className={styles.anchorlink} offset={50} href="#contact">Connect With Me</AnchorLink></div>
+        <div className={styles.navconnect}>
+          {" "}
+          <AnchorLink className={styles.anchorlink} offset={50} href="#contact">
+            Connect With Me
+          </AnchorLink>
+        </div>
       </div>
     </>
   );
